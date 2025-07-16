9ddaceb7c491cb3a4c771848a71accfe4b001c65
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-07-16 15:06:40 +0900

    truck-factorの動作確認

--- src/main1.sh
+++ src/main1.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# OSS-fuzzからオープンソースプロジェクトを抽出し、クローンする
-#https://github.com/google/oss-fuzz/tree/master/projects
-# truck-factorを使用してコア開発者情報を抽出する
+# OSS-fuzzからオープンソースプロジェクトを抽出する
+
+#ファイルがある場所をカレントディレクトリに設定
+cd "$(dirname "$0")"
+
+cd ../oss-fuzz/projects
+
+# 各サブディレクトリをループして project.yaml から main_repo を抽出
+for dir in */; do
+	yaml="$dir/project.yaml"
+	#yamlファイル内にmain_repo: が存在する場合、そのURLを表示する
+	if [ -f "$yaml" ]; then
+	url=$(grep '^main_repo:' "$yaml" | awk '{print $2}')
+	echo $url
+	fi
+done
---
+++ src/main2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ossをクローンする
+# truck-factorを使用してコア開発者情報を抽出する
+
+oss_list=(
+"https://github.com/abseil/abseil-py"
+#"https://github.com/ada-url/ada.git"
+#"https://github.com/AzureAD/azure-activedirectory-library-for-python"
+#"https://github.com/aio-libs/aiohttp"
+)
+
+#ファイルがある場所をカレントディレクトリに設定
+cd "$(dirname "$0")"
+cd ../truck-factor/gittruckfactor
+mvn package
+
+# 各OSSをクローンしてtruck-factorを実行
+for oss in "${oss_list[@]}"; do
+	dir_name=$(basename "$oss" .git)
+	repo_full_name=$(echo "$oss" | sed -E 's/.*\.com\/(.*)/\1/')
+	echo "dir_name: $dir_name, repo_full_name: $repo_full_name"
+
+	if [ ! -d "$dir_name" ]; then
+		git clone "$oss"
+	fi
+
+	( cd scripts && ./commit_log_script.sh ../"$dir_name" )
+	java -jar target/gittruckfactor-1.0.jar "$dir_name" "$repo_full_name"
+done
