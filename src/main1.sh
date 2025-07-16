da373400be8981a2481ae4ff8dcc2482e24ad92c
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-07-16 23:58:50 +0900

    fix main1.sh

--- src/main1.sh
+++ src/main1.sh
@@ -16,13 +16,14 @@ for dir in */; do
 	yaml="$dir/project.yaml"
 	#yamlファイル内にmain_repo: が存在する場合、そのURLを表示する
 	if [ -f "$yaml" ]; then
-	url=$(grep '^main_repo:' "$yaml" | awk '{print $2}')
-	#'や"で囲まれている場合はそれを除去
-	url=$(echo "$url" | sed -e 's/^["'\'']//;s/["'\'']$//')
-	#拡張子が .git の場合は除去
-	url=$(echo "$url" | sed 's/\.git$//')
-	#urlが"https://github.com/"で始まる場合のみ表示
-	if [[ "$url" == https://github.com/* ]]; then
-		echo "$url"
+		url=$(grep '^main_repo:' "$yaml" | awk '{print $2}')
+		#'や"で囲まれている場合はそれを除去
+		url=$(echo "$url" | sed -e 's/^["'\'']//;s/["'\'']$//')
+		#拡張子が .git の場合は除去
+		url=$(echo "$url" | sed 's/\.git$//')
+		#urlが"https://github.com/"で始まる場合のみ表示
+		if [[ "$url" == https://github.com/* ]]; then
+			echo "$url"
+		fi
 	fi
 done
