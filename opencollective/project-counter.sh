616567254ad795ad1d0efe66d636e10678203e26
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-10-03 17:03:12 +0900

    Update project-counter.sh

--- opencollective/project-counter.sh
+++ opencollective/project-counter.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#api-key.txtから読み込む
+MY_TOKEN=$(cat api-key.txt)
+
+#プロジェクトの個数を数える
 curl https://api.opencollective.com/graphql/v2 \
 	-H "Content-Type: application/json" \
+	-H "Personal-Token: $MY_TOKEN" \
 	-d '{"query":"query{ accounts(limit:1, offset:0, type:[PROJECT]){ totalCount } }"}'
