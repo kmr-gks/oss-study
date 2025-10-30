a6d074f1c917ad6134f8e20ab48a3acee184d216
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-10-30 16:02:49 +0900

    extract slag

--- opencollective/fund-plot.py
+++ opencollective/fund-plot.py
@@ -1,27 +1,22 @@
-import psycopg2
 import api
 import pandas as pd
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+
+project_slug = "legal-fees"
 
 # --- DB接続情報 ---
-conn = psycopg2.connect(
-    dbname="opencollective",
-    user="postgres",
-    password=api.load_sql_password_from_credentials(),
-    host="localhost",
-    port=5432
-)
+engine= create_engine('postgresql+psycopg2://postgres:{}@localhost:5432/opencollective'.format(api.load_sql_password_from_credentials()))
 
 # --- SQL実行 ---
-query = """
+query = f"""
 SELECT DATE(created_at) AS date, SUM(amount_value) AS total_amount
 FROM transactions
-WHERE project_slug = 'legal-fees' AND type = 'CREDIT'
+WHERE project_slug = '{project_slug}' AND type = 'CREDIT'
 GROUP BY DATE(created_at)
 ORDER BY DATE(created_at);
 """
-df = pd.read_sql(query, conn)
-conn.close()
+df=pd.read_sql(query, engine)
 
 # --- データ確認 ---
 print(df.head())
@@ -34,7 +29,7 @@ if not df.empty:
     plt.figure(figsize=(10,5))
     plt.plot(df["date"], df["total_amount"], marker="o", label="Daily received amount")
     plt.axhline(y=median_value, color='red', linestyle='--', label=f"Median = {median_value:.2f}")
-    plt.title("Daily Funding Received by legal-fees")
+    plt.title("Daily Funding Received by {}".format(project_slug))
     plt.xlabel("Date")
     plt.ylabel("Amount")
     plt.legend()
