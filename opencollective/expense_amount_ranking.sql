5c8e16ea82c921b90155d00989cd7b99f1e78ebc
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-12-26 14:10:25 +0900

    Add count column and asc sort

--- opencollective/expense_amount_ranking.sql
+++ opencollective/expense_amount_ranking.sql
@@ -4,20 +4,33 @@ set client_encoding to UTF8;
 -- 1) to_account_type 別：支出総額ランキング
 SELECT
   COALESCE(to_account_type, '[null]') AS to_account_type,
+  count(*) AS count,
   SUM(amount_value) AS total_amount_value
 FROM collective_transactions
 WHERE kind = 'EXPENSE'
   AND amount_currency = 'USD'
 GROUP BY COALESCE(to_account_type, '[null]')
-ORDER BY ABS(SUM(amount_value)) DESC;
+ORDER BY total_amount_value DESC;
 
 -- 2) to_account_name 別：支出総額ランキング（上位20件）
 SELECT
   COALESCE(to_account_name, '[null]') AS to_account_name,
+  count(*) AS count,
   SUM(amount_value) AS total_amount_value
 FROM collective_transactions
 WHERE kind = 'EXPENSE'
   AND amount_currency = 'USD'
 GROUP BY COALESCE(to_account_name, '[null]')
-ORDER BY ABS(SUM(amount_value)) DESC
+ORDER BY total_amount_value ASC
+LIMIT 20;
+
+SELECT
+  COALESCE(to_account_name, '[null]') AS to_account_name,
+  count(*) AS count,
+  SUM(amount_value) AS total_amount_value
+FROM collective_transactions
+WHERE kind = 'EXPENSE'
+  AND amount_currency = 'USD'
+GROUP BY COALESCE(to_account_name, '[null]')
+ORDER BY total_amount_value DESC
 LIMIT 20;
